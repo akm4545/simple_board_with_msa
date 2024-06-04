b2882993cb4700c0636e3142cc06124848610494
akm4545 <akm4545@naver.com>
2024-06-04 11:51:50 +0900

    @Rrfresh 어노테이션 추가 및 테스트

--- backend/user-service/src/main/java/com/simpleboard/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/simpleboard/userservice/UserServiceApplication.java
@@ -2,8 +2,13 @@ package com.simpleboard.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+//액추에이터가 생성해주는 /refresh 엔드포인트로 컨피그 서버의 데이터 갱신
+//DB 접속정보는 갱신되지 않는다
+//application.yml에서 액추에이터 endpoint를 노출시켜야 한다
+@RefreshScope
 public class UserServiceApplication {
 
     public static void main(String[] args) {
