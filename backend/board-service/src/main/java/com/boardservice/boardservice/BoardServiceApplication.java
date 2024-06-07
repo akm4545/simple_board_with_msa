edc2b0fcd50753a32ec9dbe7066e8136b2261f32
akm4545 <akm4545@naver.com>
2024-06-07 17:04:56 +0900

    feignClient 의존성 추가 및 어노테이션 설정

--- backend/board-service/src/main/java/com/boardservice/boardservice/BoardServiceApplication.java
+++ backend/board-service/src/main/java/com/boardservice/boardservice/BoardServiceApplication.java
@@ -3,12 +3,14 @@ package com.boardservice.boardservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //액추에이터가 생성해주는 /refresh 엔드포인트로 컨피그 서버의 데이터 갱신
 //DB 접속정보는 갱신되지 않는다
 //application.yml에서 액추에이터 endpoint를 노출시켜야 한다
 @RefreshScope
+@EnableFeignClients
 public class BoardServiceApplication {
 
     public static void main(String[] args) {
