007812f602c2536e8da50782ab4c2e377bf62f81
akm4545 <akm4545@naver.com>
2024-06-10 12:40:27 +0900

    서비스 디스커버리 통신 테스트 완료

--- backend/user-service/src/main/java/com/simpleboard/userservice/dto/UserListRequestDto.java
+++ backend/user-service/src/main/java/com/simpleboard/userservice/dto/UserListRequestDto.java
@@ -1,12 +1,12 @@
 package com.simpleboard.userservice.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
 @Data
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserListRequestDto {
     private List<Integer> userSeqList;
 }
