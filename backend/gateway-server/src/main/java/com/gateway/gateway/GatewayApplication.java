ceca46af33e265a66280fde8eb722727b17094ce
akm4545 <akm4545@naver.com>
2024-06-14 12:14:05 +0900

    프로젝트 경로 오타 수정

--- backend/gateway-server/src/main/java/com/gateway/gateway/GatewayApplication.java
+++ backend/gateway-server/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -1,13 +1,14 @@
-package com.gateway.geteway;
+package com.gateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetewayApplication {
+@EnableEu
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GetewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
--- backend/gateway-server/src/test/java/com/gateway/gateway/GetewayApplicationTests.java
+++ backend/gateway-server/src/test/java/com/gateway/gateway/GetewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gateway.geteway;
+package com.gateway.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
