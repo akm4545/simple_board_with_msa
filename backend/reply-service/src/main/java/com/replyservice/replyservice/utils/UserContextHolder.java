8ddb52771ce1ad98abda65e28ac04bb451cdc613
akm4545 <akm4545@naver.com>
2024-06-18 16:37:11 +0900

    상관관계 ID 사용을 위한 클래스들 서비스에 생성

---
+++ backend/reply-service/src/main/java/com/replyservice/replyservice/utils/UserContextHolder.java
@@ -0,0 +1,31 @@
+package com.replyservice.replyservice.utils;
+
+import org.springframework.util.Assert;
+
+public class UserContextHolder {
+//    스레드 변수 공유를 막기 위해 동기화를 사용
+//    동기화를 피하고 싶을 때 ThreadLocal 변수를 사용
+    private static final ThreadLocal<UserContext> userContext = new ThreadLocal<UserContext>();
+
+    public static final UserContext getContext() {
+        UserContext context = userContext.get();
+
+        if(context == null){
+            context = createEmptyContext();
+
+            userContext.set(context);
+        }
+
+        return userContext.get();
+    }
+
+    public static final void setContext(UserContext context){
+        Assert.notNull(context, "Only non-null UserContext instances are permitted");
+
+        userContext.set(context);
+    }
+
+    public static final UserContext createEmptyContext() {
+        return new UserContext();
+    }
+}
---
+++ backend/user-service/src/main/java/com/simpleboard/userservice/utils/UserContextFilter.java
@@ -0,0 +1,27 @@
+package com.simpleboard.userservice.utils;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class UserContextFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
+        UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+        UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+
+        logger.debug("UserContextFilter Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+
+        chain.doFilter(httpServletRequest, response);
+    }
+}
